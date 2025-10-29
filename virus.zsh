2bfce499afc061684a29502f262686c542c410cf
greg <grzegorz.kozub@gmail.com>
2025-10-29 09:24:05 +0100

    forward

--- virus.zsh
+++ virus.zsh
@@ -14,11 +14,23 @@ popd
 
 # links
 
-cp /usr/share/applications/intune-portal.desktop $XDG_DATA_HOME/applications
-sed -i \
-  -e 's/^Name=Microsoft Intune$/Name=Intune/' \
-  -e 's/^Exec=env /Exec=env WEBKIT_DISABLE_DMABUF_RENDERER=1 /' \
-  $XDG_DATA_HOME/applications/intune-portal.desktop
+# cp /usr/share/applications/intune-portal.desktop $XDG_DATA_HOME/applications
+# sed -i \
+#   -e 's/^Name=Microsoft Intune$/Name=Intune/' \
+#   -e 's/^Exec=env /Exec=env WEBKIT_DISABLE_DMABUF_RENDERER=1 /' \
+#   $XDG_DATA_HOME/applications/intune-portal.desktop
+
+# services
+
+# systemctl status microsoft-identity-device-broker.service
+# systemctl --user status microsoft-identity-broker.service
+#
+# systemctl status intune-daemon.service
+# systemctl --user status intune-agent.timer
+# systemctl --user status intune-agent.service
+#
+# env WEBKIT_DISABLE_DMABUF_RENDERER=1 /opt/microsoft/intune/bin/intune-portal
+
 
 # cleanup
 
