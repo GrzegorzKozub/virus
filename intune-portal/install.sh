450e5ef108a464319ff5d91c172cd01c651739c6
greg <grzegorz.kozub@gmail.com>
2025-10-31 07:58:06 +0100

    forward

--- intune-portal/install.sh
+++ intune-portal/install.sh
@@ -8,3 +8,29 @@ post_install() {
 post_remove() {
   [[ -f /etc/os-release.original ]] && mv /etc/os-release.original /etc/os-release
 }
+
+post_upgrade() {
+  # Restart polkit to get any new authorization actions we put down
+  systemctl restart polkit.service 2>/dev/null
+}
+
+post_install() {
+  if [ -d /run/systemd/system ]; then
+    systemd-tmpfiles --create intune.conf
+  fi
+
+  systemctl --system daemon-reload
+  systemctl enable intune-daemon.socket --now
+
+  post_upgrade
+
+  echo "Please run: systemctl enable --now --user intune-agent.timer" 1>&2
+}
+
+pre_remove() {
+  systemctl disable intune-daemon.socket --now
+}
+
+post_remove() {
+  systemctl --system daemon-reload
+}
